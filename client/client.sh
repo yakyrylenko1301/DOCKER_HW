7516286a767d7a0333f912184f297ff436cc556c
yaroslav <yakyrylenko1301>
2024-05-13 00:09:01 +0300

    Create project

---
+++ client/client.sh
@@ -0,0 +1,5 @@
+echo "Hello world from CLIENT !!!"
+ip address
+# ifconfig
+
+while true; do echo -n 'Line of text' | nc localhost 1234 ; done
---
+++ server/server.sh
@@ -0,0 +1,6 @@
+echo "Hello world from SERVER !!!"
+ip address
+# ifconfig
+
+
+while true; do nc -u localhost 1234; done
